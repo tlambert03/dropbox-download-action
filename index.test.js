c1b79accfd57050afdaee908cefaf1557b011b5c
Talley Lambert <talley.lambert@gmail.com>
2023-06-23 13:44:02 -0400

    update

--- index.test.js
+++ index.test.js
@@ -1,8 +1,6 @@
 const dropboxDownload = require("./src/dbx");
 const process = require("process");
-const cp = require("child_process");
-const path = require("path");
 
-test("test download", async () => {
-  await dropboxDownload("/ga_test", ".", process.env.DROPBOX_TOKEN);
+test("download works", async () => {
+  dropboxDownload("/ga_test", ".", process.env.DROPBOX_TOKEN);
 });
